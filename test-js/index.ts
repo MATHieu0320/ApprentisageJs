353ae3935f09861c717c842560d43b19f2b91990
Mathie <math039@outlook.fr>
2024-05-07 20:50:20 +0200

    typescript

---
+++ test-js/index.ts
@@ -0,0 +1,12 @@
+// let chiffre:number;
+
+// chiffre = "ff"
+// let resu = 45;
+
+// resu = true
+// const sayname = (name?:string) =>{
+//     console.log("bonjour " + name);
+    
+
+// }
+// sayname(45)
